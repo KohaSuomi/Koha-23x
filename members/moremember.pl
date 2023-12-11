Koha-Community/Koha

KD-2473: GDPR logging improvements

members/moremember.pl
@@ -368,4 +368,6 @@ $template->param(
     relatives_borrowernumbers => \@relatives,
 );
 
+C4::Log::logaction("MEMBERS", "VIEW", $borrowernumber, "Details page") if C4::Context->preference("BorrowersViewLog");
+
 output_html_with_http_headers $input, $cookie, $template->output;

KohaSuomi/Koha-23x

members/moremember.pl
@@ -314,4 +314,6 @@ if ( C4::Context->preference('UseRecalls') ) {
     );
 }
 
+C4::Log::logaction("MEMBERS", "VIEW", $borrowernumber, "Patron details") if C4::Context->preference("BorrowersViewLog");
+
 output_html_with_http_headers $input, $cookie, $template->output;