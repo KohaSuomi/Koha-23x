7d9093bd3fcdf719fd9e1eeb1ed8e801e3c6d6d1
atarnus <trainee@koha-suomi.fi>
2024-04-17 13:03:40 +0300

    Fixed typo on marc field

--- cataloguing/value_builder/084a_mainheading_branchloc_genre_signum_builder.pl
+++ cataloguing/value_builder/084a_mainheading_branchloc_genre_signum_builder.pl
@@ -62,7 +62,7 @@ function Click$function_name(event) {
         // Do main heading
         // Actually we should also follow the bypass indicators here
 
-        var marc942f = resp.f942m;
+        var marc942m = resp.f942m;
 
         var marc100a = resp.f100a;
         var marc110a = resp.f110a;
