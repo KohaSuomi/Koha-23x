64c3f11061bd687d4bffdd3708065684131ce1ae
Johanna Raisa <johanna.raisa@gmail.com>
2024-05-31 08:44:27 +0300

    Remove 942 field if cn_sort update has left it empty

--- C4/Biblio.pm
+++ C4/Biblio.pm
@@ -2531,6 +2531,9 @@ sub _koha_marc_update_biblioitem_cn_sort {
         if ( $cn_sort ne '' ) {
             $field->add_subfields( $biblioitem_subfield => $cn_sort );
         }
+        if (!$field->subfields) {
+            $marc->delete_field($field);
+        }
     } else {
 
         # if we get here, no biblioitem tag is present in the MARC record, so
